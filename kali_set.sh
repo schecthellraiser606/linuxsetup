ec112befe6be229743d33a5c35e4aa3f5bfbe0c0
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2023-10-11 23:07:37 +0900

    kali

--- kali_set.sh
+++ kali_set.sh
@@ -10,7 +10,7 @@ sudo apt install steghide stegcracker -y
 sudo gem install zsteg
 
 # Ghostpack
-sudo cd /opt
+cd /opt
 sudo git clone https://github.com/r3motecontrol/Ghostpack-CompiledBinaries.git
 cd $HOME
 
@@ -25,19 +25,19 @@ cd $HOME
 sudo gem install one_gadget
 sudo python3 -m pip install pwntools
 sudo apt install gdb gdb-peda -y
-sudo cd /opt
+cd /opt
 sudo git clone https://github.com/scwuaptx/Pwngdb.git 
 cd $HOME
 
 #floss
-sudo cd /opt
+cd /opt
 sudo wget https://github.com/mandiant/flare-floss/releases/download/quantumstrand-preview7/quantumstrand-quantumstrand-preview7-linux.zip
 sudo unzip quantumstrand-quantumstrand-preview7-linux.zip
 sudo rm quantumstrand-quantumstrand-preview7-linux.zip
 cd $HOME
 
 # angr
-sudo cd /opt
+cd /opt
 sudo python3 -m venv anger
 sudo source anger/bin/activate && sudo pip install angr monkeyhex && sudo deactivate
 cd $HOME
@@ -45,14 +45,14 @@ cd $HOME
 # Volatility3
 sudo cd /opt
 sudo git clone https://github.com/volatilityfoundation/volatility3.git
-sudo cd volatility3 && sudo python3 -m venv vnev
+cd volatility3 && sudo python3 -m venv vnev
 sudo source ./vnev/bin/activate && sudo pip3 install -r requirements.txt && sudo deactivate
 cd $HOME
 
 # CMSmap
-sudo cd /opt
+cd /opt
 sudo git clone https://github.com/Dionach/CMSmap.git
-sudo cd CMSmap && sudo pip3 install .
+cd CMSmap && sudo pip3 install .
 cd $HOME
 
 #Docker
@@ -69,7 +69,7 @@ sudo mv docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
 
 # Rusthound
 # Last Run
-sudo cd /opt
+cd /opt
 sudo git clone https://github.com/NH-RED-TEAM/RustHound
-sudo cd RustHound && sudo docker build --rm -t rusthound . && sudo docker run --rm -v ./:/usr/src/rusthound rusthound linux_musl
+cd RustHound && sudo docker build --rm -t rusthound . && sudo docker run --rm -v ./:/usr/src/rusthound rusthound linux_musl
 cd $HOME
