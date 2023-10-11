94c514078d60d4e7a22a929855a9d4f3370d5877
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2023-10-11 23:23:43 +0900

    docker

--- kali_set.sh
+++ kali_set.sh
@@ -56,9 +56,10 @@ cd CMSmap && sudo pip3 install .
 cd $HOME
 
 #Docker
-printf '%s\n' "deb https://download.docker.com/linux/debian bullseye stable" | sudo tee /etc/apt/sources.list.d/docker-ce.list
+sudo apt -y install curl gnupg2 apt-transport-https software-properties-common ca-certificates
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-ce-archive-keyring.gpg
-sudo apt update && sudo apt install -y docker.io docker-ce docker-ce-cli containerd.io
+echo "deb [arch=amd64] https://download.docker.com/linux/debian buster stable" | sudo tee /etc/apt/sources.list.d/docker-ce.list
+sudo apt update && sudo apt install -y docker.io && sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo systemctl enable docker --now
 sudo usermod -aG docker $USER
 newgrp docker
