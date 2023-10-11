72e434e263577cc0bcf69fc7e4cb08f7b0fedc7a
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2023-10-12 00:32:12 +0900

    tesd

--- kali_set.sh
+++ kali_set.sh
@@ -64,7 +64,7 @@ sudo apt update
 
 curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url  | grep docker-compose-linux-x86_64 | cut -d '"' -f 4 | wget -qi -
 chmod +x docker-compose-linux-x86_64
-sudo mv docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
+sudo mv ./docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
 
 
 
