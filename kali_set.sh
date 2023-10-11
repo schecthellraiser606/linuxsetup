c54211e2c9a9949a40ffc401af0ef767651ec08c
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2023-10-12 00:51:23 +0900

    test

--- kali_set.sh
+++ kali_set.sh
@@ -64,8 +64,7 @@ sudo apt update
 
 cd /tmp
 curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url  | grep docker-compose-linux-x86_64 | cut -d '"' -f 4 | wget -qi -
-chmod +x docker-compose-linux-x86_64
-sudo mv /tmp/docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
+chmod +x docker-compose-linux-x86_64 && sudo mv /tmp/docker-compose-linux-x86_64 /usr/local/bin/docker-compose
 cd $HOME
 
 # sudo apt install -y docker-ce docker-ce-cli containerd.io docker.io
