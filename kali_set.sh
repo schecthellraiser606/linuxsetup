57347cbd867141288fa934bf8ccda05034c3ace5
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2023-10-12 01:07:00 +0900

    docker

--- kali_set.sh
+++ kali_set.sh
@@ -57,19 +57,18 @@ cd $HOME
 
 # Docker
 # Please run apt install docker-ce docker-ce-cli containerd.io docker.io
-sudo apt -y install curl gnupg2 apt-transport-https software-properties-common ca-certificates
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-ce-archive-keyring.gpg
-echo "deb [arch=amd64] https://download.docker.com/linux/debian buster stable" | sudo tee /etc/apt/sources.list.d/docker-ce.list
-sudo apt update 
+
+# sudo apt -y install curl gnupg2 apt-transport-https software-properties-common ca-certificates
+# curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-ce-archive-keyring.gpg
+# echo "deb [arch=amd64] https://download.docker.com/linux/debian buster stable" | sudo tee /etc/apt/sources.list.d/docker-ce.list
+sudo apt install -y docker.io
+sudo usermod -aG docker $USER
+newgrp docker
 
 cd /tmp
 curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url  | grep docker-compose-linux-x86_64 | cut -d '"' -f 4 | wget -qi -
 chmod +x docker-compose-linux-x86_64 && sudo mv /tmp/docker-compose-linux-x86_64 /usr/local/bin/docker-compose
 cd $HOME
-sudo apt autoremove
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker.io
-sudo usermod -aG docker $USER
-newgrp docker
 
 
 
