9094e17f29160b3bf146854e791ceca5dabcf4a2
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2023-12-31 18:30:10 +0900

    update

--- kali_arm_set.sh
+++ kali_arm_set.sh
@@ -4,7 +4,7 @@ sudo apt install python3-pip python3-venv -y
 sudo pip install uploadserver kerbrute git-dumper msoffcrypto-tool
 sudo pip install certipy-ad wsgidav
 
-sudo apt install dnsmasq urlcrazy dirsearch libreoffice bloodhound ghidra gobuster golang php-curl terminator burpsuite seclists patchelf -y
+sudo apt install dnsmasq urlcrazy dirsearch libreoffice bloodhound ghidra gobuster golang php-curl terminator burpsuite seclists -y
 
 sudo wget https://raw.githubusercontent.com/flozz/p0wny-shell/master/shell.php -O /usr/share/webshells/php/p0wny-shell.php
 
@@ -17,43 +17,9 @@ cd /opt
 sudo git clone https://github.com/r3motecontrol/Ghostpack-CompiledBinaries.git
 cd $HOME
 
-# Install RustScan
-cd /tmp
-wget https://github.com/RustScan/RustScan/releases/download/2.0.1/rustscan_2.0.1_amd64.deb
-sudo dpkg -i rustscan_2.0.1_amd64.deb && sudo rm /tmp/rustscan_2.0.1_amd64.deb
-cd $HOME
-
 # pwn_nomal
-sudo gem install one_gadget
 sudo python3 -m pip install pwntools
 sudo apt install gdb -y
-cd /opt
-sudo git clone https://github.com/apogiatzis/gdb-peda-pwndbg-gef 
-sudo git clone https://github.com/scwuaptx/Pwngdb
-cd gdb-peda-pwndbg-gef && sudo ./install.sh
-sleep 2
-sudo mv /root/gef /opt
-sudo mv /root/pwndbg /opt
-sudo mv /root/peda /opt
-sudo mv /root/peda-arm /opt
-sudo rm /root/.gdbinit
-cd /tmp && wget https://raw.githubusercontent.com/schecthellraiser606/linuxsetup/main/gdbinit
-cat /tmp/gdbinit | sudo tee /etc/gdb/gdbinit
-rm /tmp/gdbinit
-echo "alias gdb='exec gdb -q -ex init-gdb \"\$@\"'" | sudo tee -a /root/.zshrc
-echo "alias gdb='exec gdb -q -ex init-gdb \"\$@\"'" >> $HOME/.zshrc
-# pwninit
-cd /tmp
-wget https://github.com/io12/pwninit/releases/download/3.3.0/pwninit
-sudo chmod +x /tmp/pwninit && sudo mv /tmp/pwninit /usr/local/bin/pwninit
-cd $HOME
-
-#floss
-cd /opt
-sudo wget https://github.com/mandiant/flare-floss/releases/download/quantumstrand-preview7/quantumstrand-quantumstrand-preview7-linux.zip
-sudo unzip quantumstrand-quantumstrand-preview7-linux.zip && sudo mv /opt/quantumstrand ./floss 
-sudo rm quantumstrand-quantumstrand-preview7-linux.zip
-cd $HOME
 
 # angr
 cd /opt
@@ -77,10 +43,8 @@ cd $HOME
 # Docker
 sudo apt install -y docker.io
 sudo usermod -aG docker $USER # sudo newgrp docker
-cd /tmp
-curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url  | grep docker-compose-linux-x86_64 | cut -d '"' -f 4 | wget -qi -
-chmod +x docker-compose-linux-x86_64 && sudo mv /tmp/docker-compose-linux-x86_64 /usr/local/bin/docker-compose
-cd $HOME
+sudo curl -L --fail https://raw.githubusercontent.com/linuxserver/docker-docker-compose/master/run.sh -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose 
 
 # Ghidra Script
 cd /opt
@@ -94,6 +58,11 @@ cd /opt/ghidra_opt && sudo mkdir ghidra_DATA && cd ghidra_DATA
 sudo git clone https://github.com/AllsafeCyberSecurity/Ghidra_Data_Type
 cd $HOME
 
+# Rustscan
+docker pull rustscan/rustscan:1.6.0
+echo "alias rustscan='docker run -it --rm --name rustscan rustscan/rustscan:2.1.1'" | sudo tee -a /root/.zshrc
+echo "alias rustscan='docker run -it --rm --name rustscan rustscan/rustscan:2.1.1'" >> $HOME/.zshrc
+
 # jwt_tool
 cd /opt
 sudo git clone https://github.com/ticarpi/jwt_tool.git
@@ -106,5 +75,4 @@ cd /opt
 sudo git clone https://github.com/NH-RED-TEAM/RustHound.git
 # cd RustHound &&  docker build --rm -t rusthound . 
 # docker run --rm -v /opt/RustHound:/usr/src/rusthound rusthound linux_musl
-# docker run --rm -v /opt/RustHound:/usr/src/rusthound rusthound linux_x86_64
 cd $HOME
