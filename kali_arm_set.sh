75a0b58f70a383a2637a0b2b13d9fe062f31b051
tak:Yassan <89838264+schecthellraiser606@users.noreply.github.com>
2024-01-07 15:49:28 +0900

    Update kali_arm_set.sh

--- kali_arm_set.sh
+++ kali_arm_set.sh
@@ -4,7 +4,7 @@ sudo apt install python3-pip python3-venv -y
 sudo pip install uploadserver kerbrute git-dumper msoffcrypto-tool
 sudo pip install certipy-ad wsgidav
 
-sudo apt install dnsmasq urlcrazy dirsearch libreoffice bloodhound ghidra gobuster golang php-curl terminator burpsuite seclists  -y
+sudo apt install dnsmasq urlcrazy dirsearch libreoffice bloodhound ghidra gobuster golang php-curl terminator burpsuite seclists ibus-anthy -y
 
 sudo wget https://raw.githubusercontent.com/flozz/p0wny-shell/master/shell.php -O /usr/share/webshells/php/p0wny-shell.php
 
