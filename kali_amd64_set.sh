22a048fb3f45344c85b021ab4c3bc82d8d4e26d7
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2024-03-01 14:01:03 +0900

    GodPoteto

--- kali_amd64_set.sh
+++ kali_amd64_set.sh
@@ -10,7 +10,7 @@ sudo apt install wine shellter -y
 sudo wget https://raw.githubusercontent.com/flozz/p0wny-shell/master/shell.php -O /usr/share/webshells/php/p0wny-shell.php
 sudo wget https://github.com/mitchmoser/SharpShares/releases/download/v2.4/SharpShares.exe -O /usr/share/windows-resources/binaries/SharpShares.exe
 sudo wget https://github.com/BeichenDream/SharpToken/releases/download/V1.2/SharpToken.exe -O /usr/share/windows-resources/binaries/SharpToken.exe
-
+sudo wget https://github.com/BeichenDream/GodPotato/releases/download/V1.20/GodPotato-NET4.exe -O /usr/share/windows-resources/binaries/GodPotato-NET4.exe
 # steg
 sudo apt install steghide stegcracker -y
 sudo gem install zsteg
