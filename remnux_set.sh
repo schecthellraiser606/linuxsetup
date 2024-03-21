d514501984abb13cf2084e330cf10f208343e55b
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2024-03-21 17:31:21 +0900

    docker

--- remnux_set.sh
+++ remnux_set.sh
@@ -56,8 +56,3 @@ cd $HOME
 # Docker
 sudo apt install -y docker.io
 sudo usermod -aG docker $USER
-# sudo newgrp docker
-cd /tmp
-curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url  | grep docker-compose-linux-x86_64 | cut -d '"' -f 4 | wget -qi -
-chmod +x docker-compose-linux-x86_64 && sudo mv /tmp/docker-compose-linux-x86_64 /usr/local/bin/docker-compose
-cd $HOME
