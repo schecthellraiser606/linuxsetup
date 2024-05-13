253e2a33684842359fe62e16a5eef82997a1c56e
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2024-05-13 13:24:36 +0900

    sqlmc

--- kali_amd64_set.sh
+++ kali_amd64_set.sh
@@ -2,7 +2,7 @@ sudo apt update && sudo apt upgrade -y
 
 sudo apt install python3-pip python3-venv -y
 sudo pip install uploadserver kerbrute git-dumper msoffcrypto-tool
-sudo pip install certipy-ad wsgidav
+sudo pip install certipy-ad wsgidav sqlmc
 
 sudo apt install dnsmasq urlcrazy dirsearch libreoffice bloodhound ghidra gobuster golang php-curl terminator burpsuite seclists patchelf mingw-w64 sshuttle xxd peass jd-gui keepassx powershell-empire -y
 sudo apt install wine shellter qemu-user -y
--- kali_arm_set.sh
+++ kali_arm_set.sh
@@ -2,7 +2,7 @@ sudo apt update && sudo apt upgrade -y
 
 sudo apt install python3-pip python3-venv -y
 sudo pip install uploadserver kerbrute git-dumper msoffcrypto-tool
-sudo pip install certipy-ad wsgidav
+sudo pip install certipy-ad wsgidav sqlmc
 
 sudo apt install dnsmasq urlcrazy dirsearch libreoffice bloodhound ghidra gobuster golang php-curl terminator seclists sshuttle peass jd-gui keepassx qemu-user powershell-empire -y
 
