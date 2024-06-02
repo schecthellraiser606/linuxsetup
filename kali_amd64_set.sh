4775cb592f19038884d4dcb8edcdbb8d0802de9b
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2024-06-02 20:28:38 +0900

    s

--- kali_amd64_set.sh
+++ kali_amd64_set.sh
@@ -11,6 +11,7 @@ sudo wget https://raw.githubusercontent.com/flozz/p0wny-shell/master/shell.php -
 sudo wget https://github.com/mitchmoser/SharpShares/releases/download/v2.4/SharpShares.exe -O /usr/share/windows-resources/binaries/SharpShares.exe
 sudo wget https://github.com/BeichenDream/SharpToken/releases/download/V1.2/SharpToken.exe -O /usr/share/windows-resources/binaries/SharpToken.exe
 sudo wget https://github.com/BeichenDream/GodPotato/releases/download/V1.20/GodPotato-NET4.exe -O /usr/share/windows-resources/binaries/GodPotato-NET4.exe
+sudo wget https://github.com/SnaffCon/Snaffler/releases/download/1.0.150/Snaffler.exe -O /usr/share/windows-resources/binaries/Snaffler.exe
 
 # steg
 sudo apt install steghide stegcracker -y
