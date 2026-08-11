dbf36311a54be2af6a7f11c35cad72ed0e4e1ca9
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2026-08-11 21:30:59 +0900

    kali_2024_amd64_set.sh and kali_2024_arm_set.sh: Add openjdk-11-jdk to the installation of additional packages

--- kali_2024_amd64_set.sh
+++ kali_2024_amd64_set.sh
@@ -10,7 +10,7 @@ pipx install certipy-ad wsgidav sqlmc
 
 sudo apt install dnsmasq urlcrazy dirsearch libreoffice bloodhound ghidra gobuster golang php-curl terminator burpsuite seclists patchelf mingw-w64 sshuttle xxd peass jd-gui keepassx powershell-empire snmp-mibs-downloader netexec hydra medusa donut bloodhound-ce-python -y
 sudo apt install wine qemu-user cadaver irssi ltrace sippts pyenv jsbeautifier jq -y
-sudo apt install mariadb-client-compat libfreetype6-dev ntpdate autorecon bloodyad faketime krb5-pkinit -y
+sudo apt install mariadb-client-compat libfreetype6-dev ntpdate autorecon bloodyad faketime krb5-pkinit openjdk-11-jdk -y
 sudo apt install kali-wallpapers-2023 -y
 
 sudo wget https://raw.githubusercontent.com/flozz/p0wny-shell/master/shell.php -O /usr/share/webshells/php/p0wny-shell.php
